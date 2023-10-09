87054d888ef3c14e68d65adc19e6b200d3700db6
sigurn2 <1250476871@qq.com>
2023-10-09 14:39:48 +0800

    import encoding module

---
+++ go.mod
@@ -0,0 +1,9 @@
+module sigurn2/crawler
+
+go 1.18
+
+require (
+	golang.org/x/net v0.16.0
+	golang.org/x/text v0.13.0
+)
+
--- main.go
+++ main.go
@@ -1,19 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
 func main() {
-	url := "https://www.xx.cn/"
-	resp, _ := http.Get(url)
-	body, _ := ioutil.ReadAll(resp.Body)
-	numLinks := strings.Count(string(body), "<a")
-	fmt.Printf("homepage has %d links!\n", numLinks)
-	exist := strings.Contains(string(body), "gold")
-	fmt.Printf("是否存在黄金：%v\n", exist)
+	url := "https://www.chinanews.com.cn/"
+	body, err := Fetch(url)
 
+	if err != nil {
+		fmt.Printf("read content failed:%v", err)
+		return
+	}
+
+	fmt.Println(string(body))
+}
+
+func Fetch(url string) ([]byte, error) {
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code:%d", resp.StatusCode)
+	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
+
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+
+	bytes, err := r.Peek(1024)
+
+	if err != nil {
+		fmt.Println("fetch error:%v", err)
+		return unicode.UTF8
+	}
+
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
