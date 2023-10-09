3fb51d850d6d2007817daeba127435966589ef2f
sigurn2 <1250476871@qq.com>
2023-10-09 12:07:10 +0800

    print resp

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	url := "https://www.xx.cn/"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("fetch url error:%v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code: %v", resp.StatusCode)
+		return
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("read content failed:%v", err)
+		return
+	}
+	fmt.Println("body:", string(body))
+}
