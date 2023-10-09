2ab379b1919e55df61e989ad65f0c706b047a6aa
sigurn2 <1250476871@qq.com>
2023-10-09 14:13:11 +0800

    second tag

--- main.go
+++ main.go
@@ -13,4 +13,7 @@ func main() {
 	body, _ := ioutil.ReadAll(resp.Body)
 	numLinks := strings.Count(string(body), "<a")
 	fmt.Printf("homepage has %d links!\n", numLinks)
+	exist := strings.Contains(string(body), "gold")
+	fmt.Printf("是否存在黄金：%v\n", exist)
+
 }
