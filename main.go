772aaf55b820611396e7d944d72dec9d1a09136d
sigurn2 <1250476871@qq.com>
2023-10-09 13:44:20 +0800

    output homepage link

--- main.go
+++ main.go
@@ -4,24 +4,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	url := "https://www.xx.cn/"
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Printf("fetch url error:%v\n", err)
-		return
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code: %v", resp.StatusCode)
-		return
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("read content failed:%v", err)
-		return
-	}
-	fmt.Println("body:", string(body))
+	resp, _ := http.Get(url)
+	body, _ := ioutil.ReadAll(resp.Body)
+	numLinks := strings.Count(string(body), "<a")
+	fmt.Printf("homepage has %d links!\n", numLinks)
 }
